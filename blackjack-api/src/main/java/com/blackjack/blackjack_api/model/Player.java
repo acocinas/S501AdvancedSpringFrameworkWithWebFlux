03e015aef512169e1ccf61caebdf86b67cd6a1b3
acocinas <alcogar79@gmail.com>
2025-05-20 13:18:27 +0200

    Add Player model mapped to MySQL table with R2DBC

---
+++ blackjack-api/src/main/java/com/blackjack/blackjack_api/model/Player.java
@@ -0,0 +1,22 @@
+package com.blackjack.blackjack_api.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("players")
+
+public class Player {
+
+    @Id
+    private Long id;
+
+    @Column("name")
+    private String name;
+
+    @Column("score")
+    private Integer score;
+}
